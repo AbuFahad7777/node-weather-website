f0771b2ccdcb11f8a26d03a976a76079cfb0adf8
stc <stc@stcs-MacBook-Pro.local>
2021-09-07 14:26:17 +0300

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define path for a Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -103,6 +104,6 @@ app.get('*', (req, res) => { // --> /* it's mean anything
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000.');
+app.listen(port, () => {
+    console.log('Server running on port ' + port + '.');
 });
